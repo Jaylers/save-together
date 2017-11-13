a7eab53bc5c2a4a28a32ebe39f556862ec6b9643
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2017-11-14 01:52:07 +0700

    background dynamic change

--- app/src/main/java/me/srichomthong/savetogether/center/SignInActivity.java
+++ app/src/main/java/me/srichomthong/savetogether/center/SignInActivity.java
@@ -1,25 +1,52 @@
 package me.srichomthong.savetogether.center;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import me.srichomthong.savetogether.R;
 import me.srichomthong.savetogether.SplashActivity;
-import me.srichomthong.savetogether.customer.CustomerMainActivity;
+import me.srichomthong.savetogether.utility.manager.ColorManager;
+import me.srichomthong.savetogether.utility.sharedpreference.SharedSignedUser;
+import me.srichomthong.savetogether.utility.sharedstring.SharedFlag;
 
 public class SignInActivity extends AppCompatActivity {
+
+    ColorManager colorManager;
+    SharedFlag sharedFlag;
+    @BindView(R.id.sign_in_txt_title) TextView text_title;
+    @BindView(R.id.sign_in_container) ConstraintLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_base_sign_in);
         ButterKnife.bind(this);
+        colorManager = new ColorManager();
+        sharedFlag = new SharedFlag();
+        whoImI();
+    }
 
+    private void whoImI(){
+        SharedSignedUser sharedSignedUser = new SharedSignedUser(SignInActivity.this);
+        if (sharedSignedUser.getTypeOfUser() == SharedFlag.flag_customer){
+            text_title.setText(getString(R.string.auth_message_im_consumer));
+            background.setBackground(colorManager.getColorDrawable(colorManager.parser(sharedFlag.flag_customer_color_theme)));
+        }else if (sharedSignedUser.getTypeOfUser() == SharedFlag.flag_restaurant){
+            text_title.setText(getString(R.string.auth_message_im_the_restaurant));
+            background.setBackground(colorManager.getColorDrawable(colorManager.parser(sharedFlag.flag_restaurant_color_theme)));
+        }else {
+            Intent intent = new Intent(SignInActivity.this, SplashActivity.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 
     @OnClick(R.id.img_google_sign_in) public void onGoogleSignIn(){
--- app/src/main/java/me/srichomthong/savetogether/utility/sharedstring/SharedFlag.java
+++ app/src/main/java/me/srichomthong/savetogether/utility/sharedstring/SharedFlag.java
@@ -12,4 +12,7 @@ public class SharedFlag {
     public static String flag_customer =                "CUSTOMER";
     public static String flag_restaurant =              "RESTAURANT";
     public static String flag_unknown =                 "UNKNOWN";
+
+    public static String flag_customer_color_theme =    "#F1C40F";
+    public static String flag_restaurant_color_theme =  "#E26A6A";
 }
